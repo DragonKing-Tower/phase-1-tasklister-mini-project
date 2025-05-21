840061435eb6cdbd4b759f832f38ac61b604f979
Cyril Rafalovich <replanttrees@gmail.com>
2025-05-20 15:11:28 -1000

    This is a submission, no formdata usage, was almost done, so...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	const form = document.querySelector("#create-task-form");
+
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+		const taskInput = document.getElementById("new-task-description").value;
+		buildToDo(taskInput);
+	});
+
+	function buildToDo(describeTask) {
+		const listedTask = document.createElement("li");
+		listedTask.textContent = describeTask;
+		document.querySelector("#tasks").append(listedTask);
+	}
 });
